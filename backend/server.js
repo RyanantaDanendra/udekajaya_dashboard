b0fb5dd087c55ce761a3529ac119a2c672c6a074
Ryananta Danendra <ryanantadanendra887@gmail.com>
2025-09-28 16:51:47 +0800

    Fixing server.js structure

--- backend/server.js
+++ backend/server.js
@@ -21,17 +21,37 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    // listen request
-    // app.listen(process.env.PORT, () => {
+const connectDb = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to db");
-    // });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
+
+// LOCAL
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     // listen request
+//     // app.listen(process.env.PORT, () => {
+//     console.log("Connected to db");
+//     // });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 app.use("/foods", foodRoutes);
 app.use("/", userRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Backend Serverless Function is running!" });
+});
+
+module.exports = async (req, res) => {
+  await connectDb();
+  // We explicitly call the express app to handle the request/response
+  app(req, res);
+};
